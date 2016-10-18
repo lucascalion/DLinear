57f678e3f49cc6e4ef6f1caada9bce395b790b9b
LucasCalion <lucascalion@gmail.com>
2016-10-18 18:13:46 -0300

    Partes Móveis

---
+++ js/dlinear.js
@@ -0,0 +1,474 @@
+  var GRAD_SPEED = 0.15;
+  var BAR_SPEED = 0.35;
+  var env = {
+      anim: 0,
+      size: 500,
+      coef: 0,
+      start: false,
+      curr: 0,
+      max: 0
+  };
+  var bar_pos = {
+      x: 219,
+      y: 160
+  };
+  var metals = [{
+      size_b: "images/barra_aco_b.png",
+      size_c: "images/barra_aco_c.png",
+      size_d: "images/barra_aco_d.png",
+      coef: 22e-6,
+  }, {
+      size_b: "images/barra_cobre_b.png",
+      size_c: "images/barra_cobre_c.png",
+      size_d: "images/barra_cobre_d.png",
+      coef: 17e-6,
+  }, {
+      size_b: "images/barra_latao_b.png",
+      size_c: "images/barra_latao_c.png",
+      size_d: "images/barra_latao_d.png",
+      coef: 15e-6,
+  }];
+  var bg_offset = {
+      x: 0,
+      y: 0
+  };
+
+  var plug_targets = [{
+      name: 'b',
+      img: "images/conector_b.png",
+      size: 500,
+      x: 744,
+      y: 256
+  }, {
+      name: 'c',
+      img: "images/conector_c.png",
+      size: 400,
+      x: 749,
+      y: 256
+  }, {
+      name: 'd',
+      img: "images/conector_d.png",
+      size: 300,
+      x: 658,
+      y: 256
+  }];
+
+  $(document)
+      .ready(function() {
+
+          var stage = new createjs.Stage("canvas");
+          $("#scale").val("3");
+          $("#scale").on("change", function() {
+              var children = stage.children;
+              for (var i = 0; i < children.length; i++) {
+                  children[i].scaleX = children[i].scaleY = getScale($(this).val());
+              }
+          });
+          $("#btn_start").on("click", function() {
+              if (env.start)
+                  return;
+              env.start = true;
+              disableControlls();
+          });
+
+          var bg_container = new createjs.Container();
+          var med_container = new createjs.Container();
+          var mbar_container = new createjs.Container();
+          var bar_container = new createjs.Container();
+          var plug_container = new createjs.Container();
+          mbar_container.tickChildren = plug_container.tickChildren = bg_container.tickChildren = false;
+          var grad_container = new createjs.Container();
+          var view_container = new createjs.Container();
+          stage.addChildAt(bg_container, 0);
+          stage.addChildAt(med_container, 1);
+          stage.addChildAt(mbar_container, 2);
+          stage.addChildAt(bar_container, 3);
+
+          stage.addChildAt(grad_container, 4);
+          stage.addChildAt(plug_container, 5);
+          stage.addChildAt(view_container, 6);
+
+          stage.mouseMoveOutside = true;
+          var bg = new createjs.Bitmap("images/regua.png");
+          bg_container.y = 100;
+          bg_container.x = 6;
+          bg_offset.y = bg_container.y;
+          bg_offset.x = bg_container.x;
+          bg_container.addChild(bg);
+
+          setupDragBtn(stage, plug_container);
+          setupDragBar(stage, mbar_container);
+          setupDragMed(stage, med_container);
+          setupDragMBar(stage, bar_container);
+          setupDragGrad(stage, grad_container);
+          setupDragView(stage, view_container);
+
+          createjs.Ticker.addEventListener("tick", function(event) {
+              stage.update(event);
+          });
+
+          $("#scale").trigger("change");
+          $('#metal_size').trigger("change");
+          $('#metal_type').trigger("change");
+
+
+      });
+
+  setupDragBtn = function(stage, layer) {
+
+      var button = null;
+      $('#metal_size').on('change', function() {
+          if (env.start)
+              return;
+          var val = $(this).val();
+          var point = plug_targets[val];
+          if(button) {
+            layer.removeChild(button);
+          }
+          button = new createjs.Bitmap(point.img);
+          layer.addChild(button);
+          layer.x = point.x;
+          layer.y = point.y;
+          env.size = point.size;
+          $('#metal_type').trigger("change");
+      });
+      // return button;
+      // var dragger_button = new createjs.Container();
+      // dragger_button.addChild(button);
+      // stage.addChild(dragger_button);
+      // dragger_button.x = bg_offset.x + 936.5;
+      // dragger_button.y = bg_offset.y + 157;
+      // dragger_button.on("mousedown", function(evt) {
+      //     if (true)
+      //         return;
+      //     var ct = evt.currentTarget,
+      //         local = ct.globalToLocal(evt.stageX, evt.stageY),
+      //         nx = ct.regX - local.x,
+      //         ny = ct.regY - local.y;
+      //     //set the new regX/Y
+      //     ct.regX = local.x;
+      //     ct.regY = local.y;
+      //     //adjust the real-position, otherwise the new regX/Y would cause a jump
+      //     ct.x -= nx;
+      //     ct.y -= ny;
+      // });
+      //
+      // dragger_button.on("pressmove", function(evt) {
+      //     // currentTarget will be the container that the event listener was added to:
+      //     evt.currentTarget.x = evt.stageX;
+      //     evt.currentTarget.y = evt.stageY;
+      //     console.log("Barra posX: " + evt.stageX + ", posY: " + evt.stageY);
+      //     // make sure to redraw the stage to show the change:
+      //     stage.update();
+      // });
+  };
+
+  setupDragBar = function(stage, layer) {
+      var bar = null;
+      $('#metal_type').on('change', function() {
+          if (env.start)
+              return;
+          var val = $(this).val();
+          var newImg = null;
+          var metal = metals[val];
+          switch (env.size) {
+            case 500:
+              newImg = metal.size_b;
+              break;
+            case 400:
+              newImg = metal.size_c;
+              break;
+            case 300:
+                newImg = metal.size_d;
+                break;
+          }
+
+          if (bar) {
+              layer.removeChild(bar);
+          }
+          bar = new createjs.Bitmap(newImg);
+          layer.addChild(bar);
+          layer.x = bg_offset.x + bar_pos.x;
+          layer.y = bg_offset.y + bar_pos.y;
+          env.coef = metal.coef;
+          //bar.scaleX = bar.scaleY = getScale($('#scale').val());
+      });
+      // var dragger_bar = new createjs.Container();
+      // dragger_bar.addChild(bar);
+      // stage.addChild(dragger_bar);
+      // dragger_bar.x = 100;
+      // dragger_bar.y = 100;
+      // dragger_bar.on("mousedown", function(evt) {
+      //   if(true) return;
+      //     var ct = evt.currentTarget,
+      //         local = ct.globalToLocal(evt.stageX, evt.stageY),
+      //         nx = ct.regX - local.x,
+      //         ny = ct.regY - local.y;
+      //     //set the new regX/Y
+      //     ct.regX = local.x;
+      //     ct.regY = local.y;
+      //     //adjust the real-position, otherwise the new regX/Y would cause a jump
+      //     ct.x -= nx;
+      //     ct.y -= ny;
+      // });
+      //
+      // dragger_bar.on("pressmove", function(evt) {
+      //     // currentTarget will be the container that the event listener was added to:
+      //     evt.currentTarget.x = evt.stageX;
+      //     evt.currentTarget.y = evt.stageY;
+      //     console.log("Barra posX: " + evt.stageX + ", posY: " + evt.stageY);
+      //     // make sure to redraw the stage to show the change:
+      //     stage.update();
+      // });
+  };
+
+  setupDragGrad = function(stage, layer) {
+      var animMatrix = [{
+          colors: ["#0000ff", "#b3b3ff"],
+          ratio: [0, 1],
+      }, {
+          colors: ["#0000ff", "#b3b3ff", "#ffff00"],
+          ratio: [0, 0.45, 1],
+      }, {
+          colors: ["#0000ff", "#b3b3ff", "#ffff00", "#cc0000"],
+          ratio: [0, 0.30, 0.45, 1],
+      }];
+      var shape = new createjs.Shape();
+      //Grad posX: 391, posY: 495.81766777941993
+      var max_w = 1190,
+          curr_w = 45;
+      shape.graphics.drawRect(0, 0, curr_w, 47);
+      shape.on("tick", function(event) {
+          if (env.start) {
+              curr_w = Math.round(curr_w + event.delta * GRAD_SPEED);
+              curr_w = curr_w > 1190 ? 1190 : curr_w;
+              var step = env.anim;
+              switch (step) {
+                  case 0:
+                      calcGradAnim(curr_w);
+                      shape.graphics.clear();
+                      shape.graphics.beginLinearGradientFill(animMatrix[step].colors, animMatrix[step].ratio, 0, 0, curr_w, 47);
+                      shape.graphics.drawRect(0, 0, curr_w, 47);
+                      shape.graphics.endFill();
+                      break;
+                  case 1:
+                      calcGradAnim(curr_w);
+                      shape.graphics.clear();
+                      shape.graphics.beginLinearGradientFill(animMatrix[step].colors, animMatrix[step].ratio, 0, 0, curr_w, 47);
+                      shape.graphics.drawRect(0, 0, curr_w, 47);
+                      shape.graphics.endFill();
+                      break;
+                  default:
+                      calcGradAnim(curr_w);
+                      step = step > 2 ? 2 : step;
+                      shape.graphics.clear();
+                      shape.graphics.beginLinearGradientFill(animMatrix[step].colors, animMatrix[step].ratio, 0, 0, curr_w, 47);
+                      shape.graphics.drawRect(0, 0, curr_w, 47);
+                      shape.graphics.endFill();
+                      //console.log(env.anim);
+                      break;
+              }
+
+              if (env.anim > 2) {
+
+                  if (env.curr === 0) {
+
+                      env.curr = env.coef * env.size * 10;
+                      env.max = env.coef * env.size * 68;
+                      //console.log("Inicial curr:"+env.curr);
+                  } else {
+                      env.curr += (event.delta * BAR_SPEED)/1000;
+                      env.curr = env.curr > env.max ? env.max : env.curr;
+                      //console.log("Curr:"+env.curr);
+                  }
+                  //set text
+                  calcBarAnim(env.curr, env.max);
+
+                  if (env.anim > 6) {
+                      env.start = false;
+                      curr_w = 45;
+                      env.anim = 0;
+                      env.curr = 0;
+                      env.max = 0;
+                      enableControlls();
+                  }
+              }
+
+          }
+      });
+      //shape.graphics.drawRect(0, 0, 1190, 47);
+      layer.addChild(shape);
+      layer.x = 391;
+      layer.y = 495.81766777941993;
+      //#0000ff, #b3b3ff
+      //#ffff00, #cc0000
+      // layer.on("mousedown", function(evt) {
+      //     if (true) return;
+      //     var ct = evt.currentTarget,
+      //         local = ct.globalToLocal(evt.stageX, evt.stageY),
+      //         nx = ct.regX - local.x,
+      //         ny = ct.regY - local.y;
+      //     //set the new regX/Y
+      //     ct.regX = local.x;
+      //     ct.regY = local.y;
+      //     //adjust the real-position, otherwise the new regX/Y would cause a jump
+      //     ct.x -= nx;
+      //     ct.y -= ny;
+      // });
+      //
+      // layer.on("pressmove", function(evt) {
+      //     // currentTarget will be the container that the event listener was added to:
+      //     evt.currentTarget.x = evt.stageX;
+      //     evt.currentTarget.y = evt.stageY;
+      //     console.log("Grad posX: " + evt.stageX + ", posY: " + evt.stageY);
+      //     // make sure to redraw the stage to show the change:
+      //     stage.update();
+      // });
+  };
+
+  setupDragMed = function(stage, layer) {
+      //Barra posX: 190, posY: 182.3190546363229
+      var med = new createjs.Bitmap("images/medidor.png");
+      layer.addChild(med);
+      layer.x = 190;
+      layer.y = 182;
+      // var dragger_med = new createjs.Container();
+      // dragger_med.addChild(med);
+      // stage.addChild(dragger_med);
+      // dragger_med.x = 190;
+      // dragger_med.y = 182;
+      // dragger_med.on("mousedown", function(evt) {
+      //     if (true)
+      //         return;
+      //     var ct = evt.currentTarget,
+      //         local = ct.globalToLocal(evt.stageX, evt.stageY),
+      //         nx = ct.regX - local.x,
+      //         ny = ct.regY - local.y;
+      //     //set the new regX/Y
+      //     ct.regX = local.x;
+      //     ct.regY = local.y;
+      //     //adjust the real-position, otherwise the new regX/Y would cause a jump
+      //     ct.x -= nx;
+      //     ct.y -= ny;
+      // });
+      //
+      // dragger_med.on("pressmove", function(evt) {
+      //     // currentTarget will be the container that the event listener was added to:
+      //     evt.currentTarget.x = evt.stageX;
+      //     evt.currentTarget.y = evt.stageY;
+      //     console.log("Barra posX: " + evt.stageX + ", posY: " + evt.stageY);
+      //     // make sure to redraw the stage to show the change:
+      //     stage.update();
+      // });
+  };
+
+  setupDragMBar = function(stage, layer) {
+      //Barra posX: 248, posY: 187.32781987358453
+      var mbar = new createjs.Bitmap("images/barra.png");
+      layer.addChild(mbar);
+      layer.x = 248;
+      layer.y = 187;
+      // var dragger_mbar = new createjs.Container();
+      // dragger_mbar.addChild(mbar);
+      // stage.addChild(dragger_mbar);
+      // dragger_mbar.x = 248;
+      // dragger_mbar.y = 187;
+      // dragger_mbar.on("mousedown", function(evt) {
+      //     if (true)
+      //         return;
+      //     var ct = evt.currentTarget,
+      //         local = ct.globalToLocal(evt.stageX, evt.stageY),
+      //         nx = ct.regX - local.x,
+      //         ny = ct.regY - local.y;
+      //     //set the new regX/Y
+      //     ct.regX = local.x;
+      //     ct.regY = local.y;
+      //     //adjust the real-position, otherwise the new regX/Y would cause a jump
+      //     ct.x -= nx;
+      //     ct.y -= ny;
+      // });
+      //
+      // dragger_mbar.on("pressmove", function(evt) {
+      //     // currentTarget will be the container that the event listener was added to:
+      //     evt.currentTarget.x = evt.stageX;
+      //     evt.currentTarget.y = evt.stageY;
+      //     console.log("Barra posX: " + evt.stageX + ", posY: " + evt.stageY);
+      //     // make sure to redraw the stage to show the change:
+      //     stage.update();
+      // });
+  };
+
+  setupDragView = function(stage, layer) {
+      var shape = new createjs.Shape();
+      //View posX: 55, posY: 154.26996930765785
+      shape.graphics.beginFill("#ff0000").drawRect(0, 0, 450, 250);
+      shape.on("tick", function(event) {
+          if (env.start) {
+          }
+      });
+      layer.addChild(shape);
+      layer.x = 55;
+      layer.y = 154;
+      // layer.on("mousedown", function(evt) {
+      //     if (true) return;
+      //     var ct = evt.currentTarget,
+      //         local = ct.globalToLocal(evt.stageX, evt.stageY),
+      //         nx = ct.regX - local.x,
+      //         ny = ct.regY - local.y;
+      //     //set the new regX/Y
+      //     ct.regX = local.x;
+      //     ct.regY = local.y;
+      //     //adjust the real-position, otherwise the new regX/Y would cause a jump
+      //     ct.x -= nx;
+      //     ct.y -= ny;
+      // });
+      //
+      // layer.on("pressmove", function(evt) {
+      //     // currentTarget will be the container that the event listener was added to:
+      //     evt.currentTarget.x = evt.stageX;
+      //     evt.currentTarget.y = evt.stageY;
+      //     console.log("View posX: " + evt.stageX + ", posY: " + evt.stageY);
+      //     // make sure to redraw the stage to show the change:
+      //     stage.update();
+      // });
+  };
+
+  disableControlls = function() {
+      $(".controll").prop('disabled', true);
+  };
+
+  enableControlls = function() {
+      $(".controll").prop('disabled', false);
+  };
+
+  calcGradAnim = function(w) {
+      if (w >= 1190) {
+          env.anim = env.anim == 2 ? 3 : env.anim;
+          return;
+      }
+
+      if (w < (1190 * 30 / 100)) {
+          env.anim = 0;
+      } else if (w > (1190 * 30 / 100) && w < (1190 * 60 / 100)) {
+          env.anim = 1;
+      } else if (w > (1190 * 60 / 100)) {
+          env.anim = 2;
+      }
+
+  };
+
+  calcBarAnim = function(curr, max) {
+      if (curr >= max) {
+          env.anim = 7;
+      }
+
+  };
+
+  getScale = function(s) {
+      switch (s) {
+          case '0':
+              return 0.09;
+          default:
+              return parseFloat(s) / 10;
+      }
+  };
