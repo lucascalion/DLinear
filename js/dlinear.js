fb15544b507525230a6b81450d75f300bbcbda51
Lucas Souza <lucascalion@gmail.com>
2016-10-25 14:18:12 -0200

    Final

--- js/dlinear.js
+++ js/dlinear.js
@@ -67,18 +67,44 @@
           $("#btn_start").on("click", function() {
               if (env.start)
                   return;
+              disableControlls(stage);
               env.start = true;
-              disableControlls();
           });
 
           var bg_container = new createjs.Container();
           var med_container = new createjs.Container();
+          med_container.name = "medidor";
+          med_container.reset = function () {
+            this.x = this.dfX;
+            this.y = this.dfY;
+          }
           var mbar_container = new createjs.Container();
+          mbar_container.name = "barra";
           var bar_container = new createjs.Container();
+          bar_container.name = "medidor_barra";
+          bar_container.reset = function () {
+            this.x = this.dfX;
+            this.y = this.dfY;
+          }
           var plug_container = new createjs.Container();
           mbar_container.tickChildren = plug_container.tickChildren = bg_container.tickChildren = false;
           var grad_container = new createjs.Container();
+          grad_container.name = "grad";
+          grad_container.reset = function () {
+            this.x = this.dfX;
+            this.y = this.dfY;
+          }
           var view_container = new createjs.Container();
+          view_container.reset = function () {
+            this.x = this.dfX;
+            this.y = this.dfY;
+          }
+          view_container.name = "view";
+          var vapor_container = new createjs.Container();
+          vapor_container.reset = function () {
+            this.alpha = 0;
+          }
+          vapor_container.name = "vapor";
           stage.addChildAt(bg_container, 0);
           stage.addChildAt(med_container, 1);
           stage.addChildAt(mbar_container, 2);
@@ -87,6 +113,7 @@
           stage.addChildAt(grad_container, 4);
           stage.addChildAt(plug_container, 5);
           stage.addChildAt(view_container, 6);
+          stage.addChildAt(vapor_container, 6);
 
           stage.mouseMoveOutside = true;
           var bg = new createjs.Bitmap("images/regua.png");
@@ -102,6 +129,7 @@
           setupDragMBar(stage, bar_container);
           setupDragGrad(stage, grad_container);
           setupDragView(stage, view_container);
+          setupDragVapor(stage, vapor_container);
 
           createjs.Ticker.addEventListener("tick", function(event) {
               stage.update(event);
@@ -191,6 +219,10 @@
           layer.x = bg_offset.x + bar_pos.x;
           layer.y = bg_offset.y + bar_pos.y;
           env.coef = metal.coef;
+          stage.getChildByName("view").reset();
+          stage.getChildByName("medidor").reset();
+          stage.getChildByName("medidor_barra").reset();
+          stage.getChildByName("grad").reset();
           //bar.scaleX = bar.scaleY = getScale($('#scale').val());
       });
       // var dragger_bar = new createjs.Container();
@@ -224,22 +256,28 @@
 
   setupDragGrad = function(stage, layer) {
       var animMatrix = [{
-          colors: ["#0000ff", "#b3b3ff"],
+          colors: ["#ffff00", "#f9b105"],
           ratio: [0, 1],
       }, {
-          colors: ["#0000ff", "#b3b3ff", "#ffff00"],
+          colors: ["#ffff00", "#f9b105", '#f94f05'],
           ratio: [0, 0.45, 1],
       }, {
-          colors: ["#0000ff", "#b3b3ff", "#ffff00", "#cc0000"],
+          colors: ["#ffff00", "#f9b105", '#f94f05', "#cc0000"],
           ratio: [0, 0.30, 0.45, 1],
       }];
       var shape = new createjs.Shape();
       //Grad posX: 391, posY: 495.81766777941993
       var max_w = 1190,
-          curr_w = 45;
+          curr_w = 45,
+          accm = 30;
       shape.graphics.drawRect(0, 0, curr_w, 47);
+      layer.reset = function () {
+        shape.graphics.clear();
+        shape.graphics.drawRect(0, 0, curr_w, 47);
+      }
       shape.on("tick", function(event) {
           if (env.start) {
+              var vapor = stage.getChildByName("vapor");
               curr_w = Math.round(curr_w + event.delta * GRAD_SPEED);
               curr_w = curr_w > 1190 ? 1190 : curr_w;
               var step = env.anim;
@@ -257,6 +295,8 @@
                       shape.graphics.beginLinearGradientFill(animMatrix[step].colors, animMatrix[step].ratio, 0, 0, curr_w, 47);
                       shape.graphics.drawRect(0, 0, curr_w, 47);
                       shape.graphics.endFill();
+                      if(vapor.alpha < 1)
+                        vapor.alpha += 0.005;
                       break;
                   default:
                       calcGradAnim(curr_w);
@@ -265,6 +305,8 @@
                       shape.graphics.beginLinearGradientFill(animMatrix[step].colors, animMatrix[step].ratio, 0, 0, curr_w, 47);
                       shape.graphics.drawRect(0, 0, curr_w, 47);
                       shape.graphics.endFill();
+                      if(vapor.alpha < 1)
+                        vapor.alpha += 0.01;
                       //console.log(env.anim);
                       break;
               }
@@ -274,23 +316,37 @@
                   if (env.curr === 0) {
 
                       env.curr = env.coef * env.size * 10;
-                      env.max = env.coef * env.size * 68;
+                      env.max = env.coef * env.size * 74;
+                      env.toText = env.curr;
                       //console.log("Inicial curr:"+env.curr);
                   } else {
                       env.curr += (event.delta * BAR_SPEED)/1000;
                       env.curr = env.curr > env.max ? env.max : env.curr;
+                      env.toText = env.curr;
                       //console.log("Curr:"+env.curr);
                   }
-                  //set text
+                  var barra = stage.getChildByName("barra");
+                  var mbarra = stage.getChildByName("medidor_barra");
+                  var medidor = stage.getChildByName("medidor");
+                  
+                  var mov = (event.delta * env.coef)*100
+                  barra.x -= mov;
+                  mbarra.x -= mov;
+                  medidor.x -= mov;
+                  if(vapor.alpha < 1)
+                    vapor.alpha += 0.25;
+                  accm+= 0.25;
+                  
                   calcBarAnim(env.curr, env.max);
 
                   if (env.anim > 6) {
                       env.start = false;
                       curr_w = 45;
+                      accm = 30;
                       env.anim = 0;
                       env.curr = 0;
                       env.max = 0;
-                      enableControlls();
+                      enableControlls(stage);
                   }
               }
 
@@ -332,6 +388,8 @@
       layer.addChild(med);
       layer.x = 190;
       layer.y = 182;
+      layer.dfX = layer.x;
+      layer.dfY = layer.y;
       // var dragger_med = new createjs.Container();
       // dragger_med.addChild(med);
       // stage.addChild(dragger_med);
@@ -368,6 +426,8 @@
       layer.addChild(mbar);
       layer.x = 248;
       layer.y = 187;
+      layer.dfX = layer.x;
+      layer.dfY = layer.y;
       // var dragger_mbar = new createjs.Container();
       // dragger_mbar.addChild(mbar);
       // stage.addChild(dragger_mbar);
@@ -399,16 +459,33 @@
   };
 
   setupDragView = function(stage, layer) {
-      var shape = new createjs.Shape();
+      var texto = new createjs.Text();
+      var txtBG = new createjs.Shape();
       //View posX: 55, posY: 154.26996930765785
-      shape.graphics.beginFill("#ff0000").drawRect(0, 0, 450, 250);
-      shape.on("tick", function(event) {
+      texto.font = "bold 126px Digital";
+      texto.color = "green";
+      texto.text = "0.00000";
+      txtBG.graphics.ss(10).s("#FFFFFF").f("#000000").rr(0, 0, 450, 250,15);
+      texto.on("tick", function(event) {
           if (env.start) {
+            if (env.anim <= 2) {
+              texto.text = "0.00000";
+            } else {
+              texto.text = env.toText.toFixed(5);
+            }
           }
       });
-      layer.addChild(shape);
+      layer.addChild(txtBG);
+      layer.addChild(texto);
+      texto.x = 90-55;
+      texto.y = 200-154;
       layer.x = 55;
       layer.y = 154;
+      layer.dfX = layer.x;
+      layer.dfY = layer.y;
+      layer.reset = function () {
+        texto.text = "0.00000";
+      }
       // layer.on("mousedown", function(evt) {
       //     if (true) return;
       //     var ct = evt.currentTarget,
@@ -433,11 +510,51 @@
       // });
   };
 
-  disableControlls = function() {
+  setupDragVapor = function(stage, layer) {
+      var texto = new createjs.Text();
+      //Vapor posX: 845, posY: 410
+      texto.font = "bold 126px Vaporized";
+      texto.color = "blue";
+      texto.text = "^ Vapor ^";
+      layer.addChild(texto);
+      layer.alpha = 0;
+      layer.x = 845;
+      layer.y = 410;
+      layer.on("mousedown", function(evt) {
+          if (true) return;
+          var ct = evt.currentTarget,
+              local = ct.globalToLocal(evt.stageX, evt.stageY),
+              nx = ct.regX - local.x,
+              ny = ct.regY - local.y;
+          //set the new regX/Y
+          ct.regX = local.x;
+          ct.regY = local.y;
+          //adjust the real-position, otherwise the new regX/Y would cause a jump
+          ct.x -= nx;
+          ct.y -= ny;
+      });
+      
+      layer.on("pressmove", function(evt) {
+          // currentTarget will be the container that the event listener was added to:
+          evt.currentTarget.x = evt.stageX;
+          evt.currentTarget.y = evt.stageY;
+          console.log("Vapor posX: " + evt.stageX + ", posY: " + evt.stageY);
+          // make sure to redraw the stage to show the change:
+          stage.update();
+      });
+  };
+
+  disableControlls = function(stage) {
+      $('#metal_type').trigger("change");
+      var mbarra = stage.getChildByName("medidor_barra");
+      mbarra.reset();
+      var medidor = stage.getChildByName("medidor");
+      medidor.reset();
       $(".controll").prop('disabled', true);
   };
 
-  enableControlls = function() {
+  enableControlls = function(stage) {
+      stage.getChildByName("vapor").reset();
       $(".controll").prop('disabled', false);
   };
 
